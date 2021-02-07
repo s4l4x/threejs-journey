85c02b1a0702b7599cdd87f42d0d398b26a19183
Alessandro Sabatelli <planoform@gmail.com>
2021-02-07 11:03:25 -0800

    Playing with transformations

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -1,6 +1,12 @@
 import "./style.css";
 import * as THREE from "three";
 
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -15,20 +21,39 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * Sizes
- */
-const sizes = {
-  width: 800,
-  height: 600,
-};
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.x = 0.2;
+// mesh.scale.y = 1;
+// mesh.scale.z = 2;
+mesh.scale.set(0.2, 1, 2);
+
+// Rotation
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = 0;
+// mesh.rotation.y = 1;
+// mesh.rotation.z = 0.2;
+mesh.rotation.set(Math.PI * 0, Math.PI * 0.35, Math.PI * -0.1, "YXZ");
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1.5);
+scene.add(axesHelper);
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+// camera.position.z = 3;
+camera.position.set(0.2, 0.3, 3);
 scene.add(camera);
+
+camera.lookAt(mesh.position);
+console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
