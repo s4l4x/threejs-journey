dcfde1514a0f8d4aa58584e4e862fe1f2989d774
Alessandro Sabatelli <planoform@gmail.com>
2022-11-18 17:56:04 +0100

    Lesson 12 - Materials

---
+++ 12-materials/src/script.js
@@ -0,0 +1,142 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * Textures
+ */
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onLoad = () => console.log("Loading complete!");
+loadingManager.onProgress = (url, itemsLoaded, itemsTotal) =>
+  console.log(
+    `Loading file: ${url}\nLoaded ${itemsLoaded} of ${itemsTotal} files.`
+  );
+loadingManager.onError = (url) =>
+  console.log("There was an error loading " + url);
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const doorAlphaTexture = textureLoader.load("./textures/door/alpha.jpg");
+const doorAOTexture = textureLoader.load(
+  "./textures/door/ambientOcclusion.jpg"
+);
+const doorColorTexture = textureLoader.load("./textures/door/color.jpg");
+const doorHeightTexture = textureLoader.load("./textures/door/height.jpg");
+const doorMetalnessTexture = textureLoader.load(
+  "./textures/door/metalness.jpg"
+);
+const doorNormalTexture = textureLoader.load("./textures/door/normal.jpg");
+const doorRoughnessTexture = textureLoader.load(
+  "./textures/door/roughness.jpg"
+);
+const gradientTexture = textureLoader.load("./textures/gradients/3.jpg");
+const matcapTexture = textureLoader.load("./textures/matcaps/1.png");
+
+/**
+ * Base
+ */
+const canvas = document.querySelector("canvas.webgl");
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const material = new THREE.MeshBasicMaterial();
+material.side = THREE.DoubleSide;
+material.map = doorColorTexture;
+// material.color.set("#ffff00");
+// material.wireframe = true;
+material.transparent = true;
+material.alphaMap = doorAlphaTexture;
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+sphere.translateX(0);
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+plane.translateX(1.5);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.4, 0.2, 16, 32),
+  material
+);
+torus.position.x = -1.5;
+
+scene.add(sphere, plane, torus);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = elapsedTime * 0.2;
+  plane.rotation.y = elapsedTime * 0.2;
+  torus.rotation.y = elapsedTime * 0.2;
+
+  sphere.rotation.x = elapsedTime * 0.1;
+  plane.rotation.x = elapsedTime * 0.1;
+  torus.rotation.x = elapsedTime * 0.1;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
