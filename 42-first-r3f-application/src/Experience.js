b48043acfc43fe4716249507733c6c36c0e68d5a
Alessandro Sabatelli <planoform@gmail.com>
2023-01-14 18:40:00 +0100

    feat: Scene updates with OrbitControls

--- 42-first-r3f-application/src/Experience.js
+++ 42-first-r3f-application/src/Experience.js
@@ -1,15 +1,22 @@
 import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { extend, useFrame, useThree } from "@react-three/fiber";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three-stdlib";
+extend({ OrbitControls });
 
 export default function Experience() {
+  const { camera, gl } = useThree();
   const cubeRef = useRef();
   const groupRef = useRef();
   useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
+    cubeRef.current.rotation.y -= 1.5 * delta;
     groupRef.current.rotation.y += delta;
   });
   return (
     <>
+      <orbitControls args={[camera, gl.domElement]} />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <group ref={groupRef}>
         <mesh
           ref={cubeRef}
@@ -18,16 +25,16 @@ export default function Experience() {
           scale={1.5}
         >
           <boxGeometry />
-          <meshBasicMaterial color="mediumpurple" wireframe />
+          <meshStandardMaterial color="mediumpurple" wireframe={false} />
         </mesh>
         <mesh position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color="orange" />
+          <meshStandardMaterial color="orange" />
         </mesh>
       </group>
-      <mesh rotation-x={Math.PI * -0.5} position-y={-2} scale={[10, 2, 1]}>
+      <mesh rotation-x={Math.PI * -0.5} position-y={-2} scale={[10, 6, 1]}>
         <planeGeometry />
-        <meshBasicMaterial color="green" />
+        <meshStandardMaterial color="green" />
       </mesh>
     </>
   );
